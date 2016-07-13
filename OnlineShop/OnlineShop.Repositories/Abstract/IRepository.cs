ba5cfd71b78b0394f613c9bb6bdea90f6a21f787
Maryan Mayher <mr.maykher@gmail.com>
2016-07-14 01:34:29 +0300

    Added base repository architecture

---
+++ OnlineShop/OnlineShop.Repositories/Abstract/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Repositories.Abstract
+{
+    public interface IRepository
+    {
+        IEnumerable<TEntity> Get<TEntity>(Func<TEntity, bool> criteria = null)  where TEntity : class;
+        TEntity GetSingle<TEntity>(Func<TEntity, bool> criteria = null) where TEntity : class;
+        TEntity Insert<TEntity>(TEntity entity) where TEntity : class;
+        void Delete<TEntity>(Func<TEntity, bool> criteria) where TEntity : class;
+        bool Exists<TEntity>(Func<TEntity, bool> criteria = null) where TEntity : class;
+    }
+}
---
+++ OnlineShop/OnlineShop.Repositories/Concrete/EFRepository.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OnlineShop.Repositories.Abstract;
+using OnlineShop.Entities;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+
+namespace OnlineShop.Repositories.Concrete
+{
+    public class EFRepository : IRepository
+    {
+        public void Delete<TEntity>(Func<TEntity, bool> criteria) where TEntity : class
+        {
+            using(var context = new ShopEntities())
+            {
+                context.Set<TEntity>().Remove(context.Set<TEntity>().SingleOrDefault<TEntity>(criteria));
+            }
+        }
+
+        public bool Exists<TEntity>(Func<TEntity, bool> criteria = null) where TEntity : class
+        {
+            TEntity resultEntity;
+            using (var context = new ShopEntities())
+            {
+                resultEntity = context.Set<TEntity>().SingleOrDefault<TEntity>(criteria);
+            }
+            return resultEntity == null ? false : true;
+        }
+
+        public IEnumerable<TEntity> Get<TEntity>(Func<TEntity, bool> criteria = null) where TEntity : class
+        {
+            using(var context = new ShopEntities())
+            {
+                IEnumerable<TEntity> query =  context.Set<TEntity>();
+
+                if(criteria != null)
+                {
+                    query = query.Where(criteria).AsQueryable();
+                }
+                return query.ToList();
+            }
+        }
+
+        public TEntity GetSingle<TEntity>(Func<TEntity, bool> criteria = null) where TEntity : class
+        {
+            using (var context = new ShopEntities())
+            {
+                IEnumerable<TEntity> query = context.Set<TEntity>();
+
+                if (criteria != null)
+                {
+                    query = query.Where(criteria).AsQueryable();
+                }
+                return query.SingleOrDefault();
+            }
+        }
+
+        public TEntity Insert<TEntity>(TEntity entity) where TEntity : class
+        {
+            TEntity insertedEntity = null;
+            using (var context = new ShopEntities())
+            {
+                insertedEntity = context.Set<TEntity>().Add(entity);
+                context.SaveChanges();
+            }
+            return insertedEntity;
+        }
+    }
+}
