b7b0c0bb16f3e251849619044ed027cf721ae06e
Maryan Mayher <mr.maykher@gmail.com>
2016-07-14 12:05:37 +0300

    Provided dependency injection using Ninject

---
+++ OnlineShop/OnlineShop.WebUI/Areas/Client/Controllers/DashboardController.cs
@@ -0,0 +1,27 @@
+﻿using OnlineShop.Entities;
+using OnlineShop.Repositories.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineShop.WebUI.Areas.Client.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private IRepository _repository;
+
+        public DashboardController(IRepository repoParam)
+        {
+            _repository = repoParam;
+        }
+
+        // GET: Client/Dashboard
+        public ActionResult Index()
+        {
+            var model = _repository.Get<Category>();
+            return View(model);
+        }
+    }
+}
