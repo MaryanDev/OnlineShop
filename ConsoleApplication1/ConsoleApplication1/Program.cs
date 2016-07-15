8aca70561c8c17260a1e3b98ee70ff9f28e899e0
Maryan Mayher <mr.maykher@gmail.com>
2016-07-16 00:56:18 +0300

    added bootstrap

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
 
 namespace ConsoleApplication1
 {
@@ -10,7 +11,20 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Maryan".Contains(""));
+            MyClass m = new MyClass();
+            Console.WriteLine(m.Field);//10
+
+            Type t = m.GetType();
+            t.GetField("_field",  BindingFlags.NonPublic | BindingFlags.Instance).SetValue(m, 15);
+
+            Console.WriteLine(m.Field);//15
         }
+    }
+
+    class MyClass
+    {
+        private int _field = 10;
+
+        public int Field { get { return _field; } }
     }
 }
